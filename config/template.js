6884e997fa56755d5ef02d67febac6e1fa020b36
mr18 <zhou060806@163.com>
2017-10-23 15:45:17 +0800

    init

---
+++ config/template.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "alias": {
+        "jq": "jquery"
+    },
+    "jquery": {
+        "git": "http://git.mucang.cn/html5page-webfront/jiakaobaodian-template-jquery.git"
+    },
+    "vue": {
+        "git": "http://git.mucang.cn/html5page-webfront/jiakaobaodian-template-vue.git"
+    },
+    "demo": {
+        "git": "http://git.mucang.cn:html5page-webfront/jiakaobaodian-vue-gulp-webpack.git"
+    }
+}
